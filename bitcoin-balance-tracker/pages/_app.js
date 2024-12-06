a08ac9d401bbd2d386e59ffd94a79dc61eb36f98
M. Kaspul Anwar <mkaspulanwar2002@gmail.com>
2024-12-06 22:49:47 +0800

    Delete UnImportant

--- bitcoin-balance-tracker/pages/_app.js
+++
@@ -1,9 +0,0 @@
-// pages/_app.js
-import './styles.css'; // Pastikan path sesuai dengan lokasi file Anda
-import React from 'react';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
